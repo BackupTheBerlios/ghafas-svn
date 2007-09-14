3397d842295554c727e3532a6f7e98c689e0c75b
tomfuks <tomfuks@3a46cede-a76f-4c1d-aebc-00018b6f7961>
2007-09-14 16:18:08 +0000

    committing local backups

--- trunk/mehdorn.py
+++ trunk/mehdorn.py
@@ -1,19 +1,21 @@
+#!/usr/bin/env python
 
-import logging, os, random, time, sys, urllib2
+import getopt, logging, os, random, time, sys, urllib2
 
-from ClientForm import ParseResponse
+import ClientForm
+import TableParse
 from sgmllib import SGMLParser
+from BeautifulSoup import BeautifulSoup
 
 
 LOGLEVEL  = logging.INFO
-LOGLEVEL  = logging.DEBUG
+#LOGLEVEL  = logging.DEBUG
 LOGFORMAT = '%(asctime)s %(levelname)s %(message)s'
 LOGFORMAT = '%(levelname)s: %(message)s'
 
 
-#                    filename='/tmp/myapp.log',
-#                    filemode='w')
 logging.basicConfig(level=LOGLEVEL, format=LOGFORMAT, stream=sys.stderr)
+#filename='/tmp/myapp.log', filemode='w'
 
 
 def tuples2dict(ts):
@@ -29,20 +31,67 @@ def sleep():
 
 def open_browser(link):
     logging.info('open link in browser: %s' % link)
-    #os.system('open "%s"' % link)
+    os.system('open "%s"' % link)
+
+def open_browser_and_exit(link):
+    open_browser(link)
+    sys.exit(1)
 
 def urlopen(url):
     logging.debug('open url: %s' % url)
     return urllib2.urlopen(url)
 
+
+class TravelData:
+    def __init__(self, fr0m, to, date, time):
+        self.fr0m = fr0m
+        self.to = to
+        self.date = date
+        self.time = time
+
+
+
+class StartPage:
+    def __init__(self):
+        response = urlopen("http://www.bahn.de/p/view/index.shtml")
+        forms = ClientForm.ParseResponse(response)
+
+        for form in forms:
+            logging.debug('form:\n' + str(form))
+
+        self.form = forms[2]
+
+        logging.debug('selected form:\n' + str(self.form))
+
+    def fill_form(self, travelData):
+        self.form['S'] = travelData.fr0m
+        self.form['Z'] = travelData.to
+        self.form['date'] = travelData.date
+        self.form['time'] = travelData.time
+        # it's a BC 50, 2. Kl
+        self.form['REQ0Tariff_TravellerReductionClass.1'] = ['4']
+
+    def submit(self):
+        logging.info('submit...')
+        response = self.form.click('submitButton')
+        return urlopen(response)
+
+
+
 class ConnectionOverviewPage(SGMLParser):
     def __init__(self, response):
         SGMLParser.__init__(self)
 
+        self.url = response.geturl()
         self.links_verfuegbarkeit = []
         self.link_spaeter = None
 
         self.feed(response.read())
+        
+    def follow_spaeter(self):
+        logging.info('follow link <Spaeter>...')
+        # return self.url + self.link_spaeter
+        return urlopen('http://reiseauskunft.bahn.de' + self.link_spaeter)
         
     def start_a(self, attrs):
         attrs = tuples2dict(attrs)
@@ -52,55 +101,99 @@ class ConnectionOverviewPage(SGMLParser):
         if 'title' in attrs and attrs['title'] == marker:
             self.links_verfuegbarkeit.append(attrs['href'])
 
-        if 'class' in attrs and attrs['class'] == 'arrowlink arrowlinkbottom':
+        marker = 'arrowlink arrowlinkbottom'
+        if 'class' in attrs and attrs['class'] == marker:
             self.link_spaeter = attrs['href']
 
+    def start_span(self, attrs):
+        self.span_tag = tuples2dict(attrs)
 
-class StartPage(SGMLParser):
-    def __init__(self):
-        response = urlopen("http://www.bahn.de/p/view/index.shtml")
-        forms = ParseResponse(response)
+    def handle_data(self, data):
+        try:
+            self.span_tag
+        except:
+            return
+        if 'class' in self.span_tag and self.span_tag['class'] == 'progress_digit_active':
+            self.ok = data == '2'
+        self.span_tag = {}
 
-        for form in forms:
-            logging.debug('form:\n' + str(form))
 
-        self.form = forms[2]
 
-        logging.debug('selected form:\n' + str(self.form))
+class ConnectionAvailabilityPage(SGMLParser):
+    def __init__(self, response):
+        SGMLParser.__init__(self)
 
-    def fill(self):
-        self.form['S'] = 'Berlin Ostbahnhof'
-        self.form['Z'] = 'Frankfurt am Main'
-        self.form['S'] = 'Fulda'
-        self.form['Z'] = 'Hof'
-        self.form['date'] = '1.12.2007'
-        self.form['time'] = '08:00'
-        self.form['REQ0Tariff_TravellerReductionClass.1'] = ['4']
+        self.url = response.geturl()
+        self.link_zurueck = None
 
-    def submit(self):
-        logging.info('submit...')
-        return urlopen(self.form.click('submitButton'))
+        self.feed(response.read())
+        
+    def follow_zurueck(self):
+        logging.info('follow link <Zurueck>...')
+        return urlopen(self.link_zurueck)
+        
+    def start_a(self, attrs):
+        attrs = tuples2dict(attrs)
+        #print attrs
+        
+        marker = 'arrowlink'
+        if 'class' in attrs and attrs['class'] == marker:
+            self.link_zurueck = attrs['href']
+
 
 
 ################################################################################
 
-startPage = StartPage()
-
-startPage.fill()
-
-overview = ConnectionOverviewPage(startPage.submit())
-
-while overview.link_spaeter:
-    logging.info('follow link: Spaeter')
-    response = urlopen('http://reiseauskunft.bahn.de' + overview.link_spaeter)
-    overview = ConnectionOverviewPage(response)
-
-
-for link in overview.links_verfuegbarkeit:
-    open_browser(link)
-    sleep()
+def get_overview(travelData):
+    startPage = StartPage()
+    startPage.fill_form(travelData)
     
+    overview = ConnectionOverviewPage(startPage.submit())
+    if not overview.ok:
+        open_browser_and_exit(overview.url)
 
+    while overview.link_spaeter:
+        response = overview.follow_spaeter()
+        overview = ConnectionOverviewPage(response)
+    
+    return overview
+
+def check_all_availabilities(overview):
+    logging.info('check all availabilities...')
+
+    for link in overview.links_verfuegbarkeit:
+        open_browser(link)
+        sleep()
+    
+def show_availability_overview(overview):
+    logging.info('show availability...')
+
+    first_link = overview.links_verfuegbarkeit[0]
+    page = ConnectionAvailabilityPage(urlopen(first_link))
+    response = page.follow_zurueck()
+    open_browser(response.geturl())
+
+            
+
+
+def main():
+    opts, args = getopt.getopt(sys.argv[1:], '', [])
+
+    if len(args) == 0:
+        args = ('Frankfurt am Main', 'Berlin hbf', '30.10.2007', '08:00')
+
+    travelData = TravelData(*args)
+
+    #check_all_availabilities(get_overview(travelData))
+    show_availability_overview(get_overview(travelData))
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+################################################################################
 
 #forms = ParseResponse(response2)
 #for form in forms:
