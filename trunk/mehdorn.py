1fdba1a92980eef1baaddb3dfad626446cfd9990
tomfuks <tomfuks@3a46cede-a76f-4c1d-aebc-00018b6f7961>
2007-09-14 16:18:13 +0000

    committing local backups

--- trunk/mehdorn.py
+++ trunk/mehdorn.py
@@ -50,6 +50,10 @@ class TravelData:
         self.time = time
 
 
+class UnexpectedPage:
+    def __init__(self, url):
+        self.url = url
+    
 
 class StartPage:
     def __init__(self):
@@ -72,105 +76,112 @@ class StartPage:
         self.form['REQ0Tariff_TravellerReductionClass.1'] = ['4']
 
     def submit(self):
-        logging.info('submit...')
+        logging.info('submit form...')
         response = self.form.click('submitButton')
         return urlopen(response)
 
 
 
-class ConnectionOverviewPage(SGMLParser):
+class TimetablePage:
     def __init__(self, response):
-        SGMLParser.__init__(self)
-
         self.url = response.geturl()
-        self.links_verfuegbarkeit = []
-        self.link_spaeter = None
+        self.links_check_availability = []
+        self.link_later = None
 
-        self.feed(response.read())
+        self.soup = BeautifulSoup(response.read())
+
+        for incident in self.soup('span'):
+            try:
+                tag_class = incident['class']
+            except KeyError:
+                continue
+            if tag_class == 'progress_digit_active':
+                self.ok = incident.contents[0] == '2'
+
+        if not self.ok:
+            raise UnexpectedPage(self.url)
+
+        for incident in self.soup('a'):
+            if incident.contents[0] == u'Verf&#252;gbarkeit pr&#252;fen':
+                link = incident['href']
+                self.links_check_availability.append(link)
+            if incident.contents[0] == u'Sp&#228;ter':
+                if not self.link_later:
+                    self.link_later = incident['href']
+    
         
-    def follow_spaeter(self):
+    def follow_link_later(self):
         logging.info('follow link <Spaeter>...')
-        # return self.url + self.link_spaeter
-        return urlopen('http://reiseauskunft.bahn.de' + self.link_spaeter)
+        # return self.url + self.link_later
+        return urlopen('http://reiseauskunft.bahn.de' + self.link_later)
         
-    def start_a(self, attrs):
-        attrs = tuples2dict(attrs)
-        #print attrs
-        
-        marker = 'Es gelten besondere Nutzungs- und Stornobedingungen.'
-        if 'title' in attrs and attrs['title'] == marker:
-            self.links_verfuegbarkeit.append(attrs['href'])
-
-        marker = 'arrowlink arrowlinkbottom'
-        if 'class' in attrs and attrs['class'] == marker:
-            self.link_spaeter = attrs['href']
-
-    def start_span(self, attrs):
-        self.span_tag = tuples2dict(attrs)
-
-    def handle_data(self, data):
-        try:
-            self.span_tag
-        except:
-            return
-        if 'class' in self.span_tag and self.span_tag['class'] == 'progress_digit_active':
-            self.ok = data == '2'
-        self.span_tag = {}
 
 
-
-class ConnectionAvailabilityPage(SGMLParser):
+class AvailabilityPage:
     def __init__(self, response):
-        SGMLParser.__init__(self)
-
         self.url = response.geturl()
-        self.link_zurueck = None
+        self.link_back = None
 
-        self.feed(response.read())
+        self.soup = BeautifulSoup(response.read())
+
+        for incident in self.soup('span'):
+            try:
+                tag_class = incident['class']
+            except KeyError:
+                continue
+            if tag_class == 'progress_digit_active':
+                self.ok = incident.contents[0] == '3'
+
+        #if not self.ok:
+        #    raise UnexpectedPage(self.url)
+
+        for incident in self.soup('a'):
+            if incident.contents[0] == u'Zur&#252;ck':
+                if not self.link_back:
+                    self.link_back = incident['href']
+
         
-    def follow_zurueck(self):
+    def follow_link_back(self):
         logging.info('follow link <Zurueck>...')
-        return urlopen(self.link_zurueck)
+        return urlopen(self.link_back)
         
-    def start_a(self, attrs):
-        attrs = tuples2dict(attrs)
-        #print attrs
         
-        marker = 'arrowlink'
-        if 'class' in attrs and attrs['class'] == marker:
-            self.link_zurueck = attrs['href']
-
 
 
 ################################################################################
 
-def get_overview(travelData):
+def request_timetable_page(travelData):
+    logging.info('request_timetable_page...')
+
     startPage = StartPage()
     startPage.fill_form(travelData)
     
-    overview = ConnectionOverviewPage(startPage.submit())
+    overview = TimetablePage(startPage.submit())
+
     if not overview.ok:
         open_browser_and_exit(overview.url)
 
-    while overview.link_spaeter:
-        response = overview.follow_spaeter()
-        overview = ConnectionOverviewPage(response)
+    while overview.link_later:
+        response = overview.follow_link_later()
+        overview = TimetablePage(response)
     
     return overview
 
-def check_all_availabilities(overview):
-    logging.info('check all availabilities...')
+def show_all_availability_pages(overview):
+    logging.info('show_all_availability_pages...')
 
-    for link in overview.links_verfuegbarkeit:
+    for link in overview.links_check_availability:
         open_browser(link)
         sleep()
     
-def show_availability_overview(overview):
-    logging.info('show availability...')
+def show_resolved_yourtimetable_page(overview):
+    logging.info('show_resolved_yourtimetable_page...')
 
-    first_link = overview.links_verfuegbarkeit[0]
-    page = ConnectionAvailabilityPage(urlopen(first_link))
-    response = page.follow_zurueck()
+    first_link = overview.links_check_availability[0]
+    print first_link
+    open_browser(first_link)
+    page = AvailabilityPage(urlopen(first_link))
+    response = page.follow_link_back()
     open_browser(response.geturl())
 
             
@@ -184,9 +195,12 @@ def main():
 
     travelData = TravelData(*args)
 
-    #check_all_availabilities(get_overview(travelData))
-    show_availability_overview(get_overview(travelData))
+    try:
+        #show_all_availability_pages(request_timetable_page(travelData))
 
+        show_resolved_yourtimetable_page(request_timetable_page(travelData))
+    except UnexpectedPage, e:
+        open_browser(e.url)
 
 
 if __name__ == "__main__":
