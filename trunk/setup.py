50c4437cceb79fb522e33eeff4796d11fac94324
tomfuks <tomfuks@3a46cede-a76f-4c1d-aebc-00018b6f7961>
2007-09-26 07:32:21 +0000

    added setup script.  taken fron sonata

---
+++ trunk/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+# $HeadURL$
+# $Id$
+
+import os
+
+from distutils.core import setup, Extension
+
+def capture(cmd):
+    return os.popen(cmd).read().strip()
+
+setup(name='GnaKB',
+        version='0.1',
+        description='GnaKB, a GTK+ client to query train connections & fares.',
+        author='tomfuks',
+        author_email='xxxxxxxxxxxx',
+        url='http://gnakb.berlios.de',
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Environment :: X11 Applications',
+            'Intended Audience :: End Users/Desktop',
+            'License :: GNU General Public License (GPL)',
+            'Operating System :: Linux',
+            'Operating System :: MacOS :: MacOS X',
+            'Programming Language :: Python',
+            'Topic :: Office/Business :: Utilities',
+            ],
+        py_modules = ['gnakb', 'tsclient'],
+        ext_modules=[],
+        scripts = ['gnakb'],
+        data_files=[('share/gnakb', ['README', 'CHANGELOG', 'TODO', 'stations.txt']),
+                    #('share/applications', ['gnakb.desktop']),
+                    #('share/pixmaps', ['pixmaps/gnakb.png', 'pixmaps/gnakb_large.png', 'pixmaps/gnakbcd.png', 'pixmaps/gnakbcd_large.png']),
+                    ],
+        )
